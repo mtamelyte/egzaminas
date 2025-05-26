80b4665d90b9da1b96c254d59bba5e22de35e82d
monikaaat <monika.tamelyte@mif.stud.vu.lt>
2025-05-26 18:28:35 +0300

    Pataisytas locale

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@
 int main()
 {
     std::locale::global(std::locale("lt_LT.UTF-8"));
-
     std::ofstream out;
+    out.imbue(std::locale());
+
     std::map<std::string, int> zodziai{};
     std::map<std::string, std::vector<int>> vietos;
     std::vector<std::string> linkai;
@@ -12,17 +13,14 @@ int main()
     ivedimas(zodziai, vietos, linkai);
     
     out.open("zodziai.txt");
-    out.imbue(std::locale());
     zodziuIsvedimas(zodziai, out);
     out.close();
 
     out.open("cross-reference.txt");
-    out.imbue(std::locale());
     crossReference(vietos, out);
     out.close();
 
     out.open("url.txt");
-    out.imbue(std::locale());
     nuoroduIsvedimas(linkai, out);
     out.close();
 }
